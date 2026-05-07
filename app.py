29adedd7d090f762d262d2296a1db3822250a1e7
EC2 Default User <ec2-user@ip-172-31-39-28.ec2.internal>
2026-05-07 15:39:57 +0000

    Added Flask web application and Docker support

--- app.py
+++ app.py
@@ -1,10 +1,72 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+states = {
+
+    "kerala": {
+        "capital": "Thiruvananthapuram",
+        "places": [
+            "Munnar",
+            "Alleppey",
+            "Kochi"
+        ]
+    },
+
+    "andhra pradesh": {
+        "capital": "Amaravati",
+        "places": [
+            "Tirupati",
+            "Vizag Beach"
+        ]
+    }
+
+}
+
+
+@app.route("/", methods=["GET"])
+
 def home():
-    return "India Tourism Flask App Running Successfully"
+
+    state = request.args.get("state")
+
+    result = ""
+
+    if state:
+
+        state = state.lower()
+
+        if state in states:
+
+            result += f"<h2>Capital: {states[state]['capital']}</h2>"
+
+            result += "<h3>Famous Places:</h3>"
+
+            for place in states[state]["places"]:
+
+                result += f"<p>{place}</p>"
+
+        else:
+
+            result = "<h2>State not found</h2>"
+
+
+    return f"""
+
+    <h1>India Tourism Project</h1>
+
+    <form>
+
+        <input type='text' name='state' placeholder='Enter State Name'>
+
+        <button type='submit'>Search</button>
+
+    </form>
+
+    {result}
+
+    """
 
 
 app.run(host="0.0.0.0", port=5000)
